75f6b6eba4d2f21e359f7f0f915003c7e12d7b17
Thom Linton <thom.linton@gmail.com>
2009-03-04 02:38:15 -0800

    replacing hackish app-level views with django.views.generic.create_update. modifying behaviour of forms.PostEditForm

--- urls.py
+++ urls.py
@@ -1,20 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic import date_based, list_detail
+from django.views.generic import date_based, create_update
 from django.views.decorators.cache import never_cache
 from django.conf.urls.defaults import *
 from django.conf import settings
-from blogy.views.generic.date_based import object_edit
+
 from blogy.forms import PostEditForm
 from blogy.models import Post
 
 
 info_dict_edit = {
-    'queryset': Post.published.all(),
-    'date_field': 'published_on',
-    'slug_field': 'slug',
-    'month_format': '%b',
     'form_class': PostEditForm,
     }
+
 info_dict_detail = {
     'queryset': Post.published.all(),
     'date_field': 'published_on',
@@ -28,14 +25,7 @@ info_dict_preview = {
     'month_format': '%b',
     'template_name': 'blogy/post_preview.html',
     }
-info_dict_preview_edit = {
-    'queryset': Post.objects.all(),
-    'date_field': 'created_on',
-    'slug_field': 'slug',
-    'month_format': '%b',
-    'form_class': PostEditForm,
-    'template_name': 'blogy/post_preview_edit.html',
-    }
+
 info_dict_year = {
     'queryset': Post.published.all(),
     'date_field': 'published_on',
@@ -48,7 +38,7 @@ info_dict_month = {
     'month_format': '%b',
     'allow_empty': True,
     }
-info_dict = {
+info_dict_archive = {
     'queryset': Post.published.all(),
     'date_field': 'published_on',
     'template_object_name': 'object_list',
@@ -57,17 +47,15 @@ info_dict = {
 
 urlpatterns = patterns('blog.views',
 
-    url(r'^$', date_based.archive_index, info_dict, name="post_list"),
+    url(r'^$', date_based.archive_index, info_dict_archive, name="post_list"),
 
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', 
         never_cache(date_based.object_detail), info_dict_detail, name="post_detail"),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/edit/$', 
-        login_required(never_cache(object_edit)), info_dict_edit, name="post_edit"),
+    url(r'^edit/(?P<object_id>\d+)/$', 
+        login_required(never_cache(create_update.update_object)), info_dict_edit, name="post_edit"),
 
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/preview/$', 
         login_required(never_cache(date_based.object_detail)), info_dict_preview, name="post_preview"),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/preview/edit/$', 
-        login_required(never_cache(object_edit)), info_dict_preview_edit, name="post_preview_edit"),
 
     url(r'^(?P<year>\d{4})/$',
         date_based.archive_year, info_dict_year, name="year_archive"),
